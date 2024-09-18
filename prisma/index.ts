0ba24bbe61afe0a1589116bf5032121e2b30ba82
Paulo Marques <paulomuniz2411@gmail.com>
2024-09-18 08:20:05 -0300

    prisma - index.ts dotenv - 18092024

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,22 +1,25 @@
 //@ts-nocheck
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 let prisma: PrismaClient;
- declare global {
-    namespace NodeJS {
-        interface Global {
-            prisma: PrismaClient
-        }
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
     }
+  }
 }
 
-if(process.env.NODE_ENV !== "production") {
-    prisma = new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+  prisma = new PrismaClient();
 } else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
