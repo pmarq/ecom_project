b86b5b4907fb221db62cf61d2c45a01e45358862
Paulo Marques <paulomuniz2411@gmail.com>
2024-09-20 08:17:09 -0300

    add navbar index.tsx force-dynamic - 20092024

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,77 +1,72 @@
-import React from 'react'
-import NavUI from './NavUi'
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { startDb } from '@/app/lib/db';
-import prisma from '@/prisma';
-import { redirect } from 'next/navigation';
+import React from "react";
+import NavUI from "./NavUi";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { startDb } from "@/app/lib/db";
+import prisma from "@/prisma";
+import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
 
 const fetchUserProfile = async () => {
-  const session = await getServerSession(authOptions) 
-  if(!session?.user) return null;
-  
-  await startDb(); 
+  const session = await getServerSession(authOptions);
+  if (!session?.user) return null;
 
-  const user = await prisma.user.findUnique({    
+  await startDb();
+
+  const user = await prisma.user.findUnique({
     where: {
-      id: session.user.id      
-    }
-  })
-  if(!user) return redirect('/auth/signin');
+      id: session.user.id,
+    },
+  });
+  if (!user) return redirect("/auth/signin");
   return {
     id: user.id.toString(),
     name: user.name ?? "",
     email: user.email ?? "",
     image: user.image ?? "",
-    emailVerified: user.emailVerified    
-  }  
-}
-
+    emailVerified: user.emailVerified,
+  };
+};
 
 const getCartItemsCount = async () => {
   try {
-    const session = await getServerSession(authOptions) 
-    if(!session?.user) return 0;
+    const session = await getServerSession(authOptions);
+    if (!session?.user) return 0;
 
-    const userId = session.user.id
+    const userId = session.user.id;
 
-    await startDb()
-   const cart = await prisma.cartDocument.findMany({
-    where: {
-      userId: userId
-    },
-    select: {
-      cartItems:{
-        select: {
-          quantity: true,
-        }
-      }
-    }   
-    
-   })
+    await startDb();
+    const cart = await prisma.cartDocument.findMany({
+      where: {
+        userId: userId,
+      },
+      select: {
+        cartItems: {
+          select: {
+            quantity: true,
+          },
+        },
+      },
+    });
 
-   let totalQuantity = 0;
-    cart.forEach(cartDocument => {
-      cartDocument.cartItems.forEach(cartItem => {
+    let totalQuantity = 0;
+    cart.forEach((cartDocument) => {
+      cartDocument.cartItems.forEach((cartItem) => {
         totalQuantity += cartItem.quantity;
       });
     });
 
-
-    return totalQuantity;    
-    
+    return totalQuantity;
   } catch (error) {
-    console.error("Error while fetching cart items count:",error);
-    return 0    
+    console.error("Error while fetching cart items count:", error);
+    return 0;
   }
-}
+};
 
 export default async function Navbar() {
-  const cartItemsCount = await getCartItemsCount()
-  const profile = await fetchUserProfile()
+  const cartItemsCount = await getCartItemsCount();
+  const profile = await fetchUserProfile();
 
-  return (
-  <NavUI cartItemsCount={cartItemsCount} avatar={profile?.image} />  
-  );
+  return <NavUI cartItemsCount={cartItemsCount} avatar={profile?.image} />;
 }
