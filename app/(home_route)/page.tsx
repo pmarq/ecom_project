b9c381a70f40873d7ada2066b1362bb684d4caea
Paulo Marques <paulomuniz2411@gmail.com>
2024-09-25 09:13:07 -0300

    home route pagetsx 2nd try 20092024

--- app/(home_route)/page.tsx
+++ app/(home_route)/page.tsx
@@ -3,11 +3,7 @@ import { startDb } from "../lib/db";
 import prisma from "@/prisma";
 import GridView from "../components/GridView";
 import ProductCard from "../components/ProductCard";
-import FeaturedProductsSlider from "../components/FeaturedProducstSlider";
-import HorizontalMenu from "../components/HorizontalMenu";
 import CategoryMenu from "../components/CategoryMenu";
-
-//como colocar uma quantidade (dentro do product table eu posso passar o fetch?? igual aqui?)
 
 interface Props {
   id: string;
@@ -27,12 +23,11 @@ const fetchLatestProducts = async () => {
   await startDb();
   const products = await prisma.product.findMany({
     where: {
-      price: { not: null as any }, //solução chat gpt**
+      price: { not: null as any },
     },
     orderBy: {
       createdAt: "desc",
     },
-
     select: {
       thumbnails: true,
       price: true,
@@ -46,18 +41,20 @@ const fetchLatestProducts = async () => {
       rating: true,
     },
   });
-  return products.map((product) => {
-    return {
-      id: product.id.toString(),
-      title: product.title,
-      description: product.description,
-      category: product.category,
-      thumbnail: product.thumbnails,
-      price: product.price,
-      sale: product.sale,
-      rating: product.rating,
-    };
-  });
+
+  return products.map((product) => ({
+    id: product.id.toString(),
+    title: product.title,
+    description: product.description,
+    category: product.category,
+    thumbnail: product.thumbnails[0].url, // Assuming the first thumbnail
+    price: {
+      base: product.price.base,
+      discounted: product.price.discounted,
+    },
+    sale: product.sale,
+    rating: product.rating,
+  }));
 };
 
 const fetchFeaturedProduct = async () => {
@@ -75,52 +72,38 @@ const fetchFeaturedProduct = async () => {
       url: true,
     },
   });
-  return featuredProducts.map((featuredProducts) => {
-    return {
-      id: featuredProducts.id.toString(),
-      title: featuredProducts.title,
-      link: featuredProducts.link,
-      linkTitle: featuredProducts.linkTitle,
-      banner: featuredProducts.url,
-    };
-  });
+  return featuredProducts.map((featuredProduct) => ({
+    id: featuredProduct.id.toString(),
+    title: featuredProduct.title,
+    link: featuredProduct.link,
+    linkTitle: featuredProduct.linkTitle,
+    banner: featuredProduct.url,
+  }));
 };
 
-let newArr: Props[] = [];
-
-export default async function Home() {
+export const getServerSideProps = async () => {
   const latestProducts = await fetchLatestProducts();
-
-  latestProducts.map((item) => {
-    const prodPrice = item?.price;
-    const strPrice = JSON.stringify(prodPrice);
-    const obj = JSON.parse(strPrice);
-
-    const newProdObj = {
-      id: item.id.toString(),
-      title: item?.title ?? "",
-      description: item?.description ?? "",
-      category: item?.category ?? "",
-      thumbnail: item?.thumbnail[0].url,
-      price: { base: obj.base, discounted: obj.discounted },
-      sale: item.sale,
-      rating: item.rating ?? 0,
-    };
-
-    newArr.push(newProdObj);
-  });
-
   const featuredProducts = await fetchFeaturedProduct();
 
+  return {
+    props: {
+      latestProducts,
+      featuredProducts,
+    },
+  };
+};
+
+const Home = ({ latestProducts }: { latestProducts: Props[] }) => {
   return (
     <div className="py-4 space-y-4">
-      {/*   <FeaturedProductsSlider products={featuredProducts} /> */}
       <CategoryMenu />
       <GridView>
-        {newArr.map((product: Props) => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
+        {latestProducts.map((product: Props) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </GridView>
     </div>
   );
-}
+};
+
+export default Home;
