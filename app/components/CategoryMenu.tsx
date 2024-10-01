07e04b8dc8343c4601ccd9b21edf3ac543e68b58
Paulo Marques <paulomuniz2411@gmail.com>
2024-10-01 11:45:03 -0300

    full categoryMenu

--- app/components/CategoryMenu.tsx
+++ app/components/CategoryMenu.tsx
@@ -3,28 +3,19 @@
 import React from "react";
 import HorizontalMenu from "./HorizontalMenu";
 import categories from "../utils/categories";
+import { Chip } from "@material-tailwind/react";
 import Link from "next/link";
-import dynamic from "next/dynamic";
-
-// Lazy load do componente Chip com SSR desativado
-const Chip = dynamic(
-  () => import("@material-tailwind/react").then((mod) => mod.Chip),
-  {
-    ssr: false, // Desativa a renderização no servidor
-  }
-);
 
 export default function CategoryMenu() {
   return (
-    /*  <HorizontalMenu> */
-    <div>
-      {categories.map((c) => (
-        <a key={c} href={`/browse-products/${c}`}>
-          <Chip color="teal" className="mr-2" variant="outlined" value={c} />
-          {c}
-        </a>
-      ))}
-    </div>
-    /*  </HorizontalMenu> */
+    <HorizontalMenu>
+      <div>
+        {categories.map((c) => (
+          <Link key={c} href={`/browse-products/${c}`}>
+            <Chip color="teal" className="mr-2" variant="outlined" value={c} />
+          </Link>
+        ))}
+      </div>
+    </HorizontalMenu>
   );
 }
