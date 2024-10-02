9b28c61e01a851de5cb6c8e104d4d9a908fb6ad2
Paulo Marques <paulomuniz2411@gmail.com>
2024-10-02 08:32:00 -0300

    auth route end vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,13 +5,11 @@ import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/prisma";
 import { SessionUserProfile, SignInCredentials } from "@/app/types";
 
-
 declare module "next-auth" {
   interface Session {
-    user: SessionUserProfile
+    user: SessionUserProfile;
   }
 }
-
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -25,52 +23,52 @@ export const authOptions: NextAuthOptions = {
       type: "credentials",
       credentials: {},
       async authorize(credentials, request) {
-        const {email , password} = credentials as SignInCredentials
+        const { email, password } = credentials as SignInCredentials;
         //send  request to your api route where you can sign in your user and send error or success response to this function.
-        
-        const {user , error} = await fetch( "http://localhost:3000/api/users/signin", {
-          method:"POST",
-          body: JSON.stringify({email, password})
-        }).then(async res => await res.json());
 
-        if(error) return null;
+        const { user, error } = await fetch(
+          "https://ecom-project-nv-evch.vercel.app/api/users/signin",
+          {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+          }
+        ).then(async (res) => await res.json());
 
-        return {id: user.id, ...user}             
+        if (error) return null;
+
+        return { id: user.id, ...user };
       },
-      
     }),
   ],
-  
-  callbacks:{
-    async jwt(params) { 
-      if(params.user){
-        params.token.user = {...params.token,...params.user}        
-      }      
-     
-      return params.token
+
+  callbacks: {
+    async jwt(params) {
+      if (params.user) {
+        params.token.user = { ...params.token, ...params.user };
+      }
+
+      return params.token;
     },
 
-   async session(params:any) {
+    async session(params: any) {
+      const user = params.token.user;
+      if (user) {
+        const userDataFromDatabase = await prisma.user.findUnique({
+          where: { id: user.id },
+          select: { emailVerified: true },
+        });
 
-    const user = params.token.user
-    if(user){
+        if (userDataFromDatabase) {
+          // Update the emailVerified status from the database
+          user.emailVerified = userDataFromDatabase.emailVerified;
+        }
 
-      const userDataFromDatabase = await prisma.user.findUnique({
-        where: { id: user.id },
-        select: { emailVerified: true }
-      });
+        params.session.user = { ...params.session.user, ...user };
+      }
 
-      if (userDataFromDatabase) {
-        // Update the emailVerified status from the database
-        user.emailVerified = userDataFromDatabase.emailVerified;      }
-
-
-     params.session.user = {...params.session.user, ...user}
-    }   
-   
-    return params.session;
+      return params.session;
     },
-  },   
+  },
   debug: process.env.NODE_ENV === "development",
   session: {
     strategy: "jwt",
