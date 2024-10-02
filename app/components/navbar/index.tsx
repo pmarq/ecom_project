09eaf832bf90f630fdf1bd76a39e46b3fc2c65d1
Paulo Marques <paulomuniz2411@gmail.com>
2024-10-02 08:10:37 -0300

    navbar 02102024

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -5,7 +5,6 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { startDb } from "@/app/lib/db";
 import prisma from "@/prisma";
 import { redirect } from "next/navigation";
-import { GetServerSidePropsContext } from "next";
 
 const fetchUserProfile = async () => {
   const session = await getServerSession(authOptions);
@@ -64,95 +63,8 @@ const getCartItemsCount = async () => {
 };
 
 export default async function Navbar() {
-  /*   const cartItemsCount = await getCartItemsCount();
-  const profile = await fetchUserProfile(); */
+  const cartItemsCount = await getCartItemsCount();
+  const profile = await fetchUserProfile();
 
-  return (
-    <div>
-      <p> NavBar</p>
-    </div>
-  );
-
-  /*  return <NavUI cartItemsCount={cartItemsCount} avatar={profile?.image} />; */
-}
-
-/* ------------------------------- */
-/* 
-interface Profile {
-  id: string;
-  name: string;
-  email: string;
-  image: string;
-}
-
-interface NavbarProps {
-  cartItemsCount: number;
-  profile: Profile;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
-
-  console.log("Session:", session);
-
-  if (!session?.user) {
-    return {
-      redirect: {
-        destination: "/auth/signin",
-        permanent: false,
-      },
-    };
-  }
-
-  await startDb();
-
-  const user = await prisma.user.findUnique({
-    where: { id: session.user.id },
-  });
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: "/auth/signin",
-        permanent: false,
-      },
-    };
-  }
-
-  const cart = await prisma.cartDocument.findMany({
-    where: {
-      userId: session.user.id,
-    },
-    select: {
-      cartItems: {
-        select: {
-          quantity: true,
-        },
-      },
-    },
-  });
-
-  let totalQuantity = 0;
-  cart.forEach((cartDocument) => {
-    cartDocument.cartItems.forEach((cartItem) => {
-      totalQuantity += cartItem.quantity;
-    });
-  });
-
-  return {
-    props: {
-      profile: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        image: user.image,
-      },
-      cartItemsCount: totalQuantity,
-    },
-  };
-}
-
-export default function Navbar({ cartItemsCount, profile }: NavbarProps) {
   return <NavUI cartItemsCount={cartItemsCount} avatar={profile?.image} />;
 }
- */
